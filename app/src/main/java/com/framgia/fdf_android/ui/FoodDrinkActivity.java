8440ca103dd8e4909f306d6e3999ca65e4b5a47e
cong tung <congtung10t2@gmail.com>
2016-10-21 16:51:02 +0700

    menu_ui

--- app/src/main/java/com/framgia/fdf_android/ui/FoodDrinkActivity.java
+++ app/src/main/java/com/framgia/fdf_android/ui/FoodDrinkActivity.java
@@ -2,9 +2,15 @@ package com.framgia.fdf_android.ui;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 import android.view.View;
 
 import com.framgia.fdf_android.R;
@@ -15,7 +21,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class FoodDrinkActivity extends AppCompatActivity implements FoodDrinkAdapter
-    .OnItemClickListener {
+    .OnItemClickListener, NavigationView.OnNavigationItemSelectedListener {
     private FoodDrinkAdapter adapter;
     private List<FoodDrinkItem> foodAndDrinks = new ArrayList<>();
     private RecyclerView recyclerView;
@@ -26,18 +32,38 @@ public class FoodDrinkActivity extends AppCompatActivity implements FoodDrinkAda
         initViews();
     }
 
+    public void initToolBar() {
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+            this, drawer, toolbar, R.string.navigation_drawer_open,
+            R.string.navigation_drawer_close);
+        drawer.setDrawerListener(toggle);
+        toggle.syncState();
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+    }
+
     public void initViews() {
-        setContentView(R.layout.activity_food_drink);
+        setContentView(R.layout.activity_menu_food_drink);
         recyclerView = (RecyclerView) findViewById(R.id.recycler_view_news);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
         adapter = new FoodDrinkAdapter(foodAndDrinks);
         adapter.setItemClickListener(this);
         recyclerView.setAdapter(adapter);
+        initToolBar();
     }
+
     @Override
     public void onItemClick(View view, int position) {
         Intent intent = new Intent(FoodDrinkActivity.this, FoodDrinkDetailActivity.class);
         intent.putExtra(Constants.ITEM_INDEX_KEY, foodAndDrinks.get(position));
         startActivity(intent);
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        return false;
     }
 }
